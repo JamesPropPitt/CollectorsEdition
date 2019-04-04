6f78c7b0523b68bc631323567b2297b6ba10a5a9
James <propthevampire@gmail.com>
2019-04-04 16:45:53 +0100

    Added login logout page to django Among other django related things

--- Jwbite/Django2-electric boogaloo/productOwner/productOwner/urls.py
+++ Jwbite/Django2-electric boogaloo/productOwner/productOwner/urls.py
@@ -1,4 +1,4 @@
-"""django_PO URL Configuration
+"""productOwner URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -11,12 +11,11 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('userPosts/', include('userPosts.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('userPosts.urls'))
 ]
